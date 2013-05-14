81a131b7666d106397052a784ced00af47f3af5b
Pelle Ravn <pelle@pelleravn.dk>
2013-05-14 14:23:16 +0200

    Lålålå

--- TheBigBank/src/thebigbank/TheBigBank.java
+++ TheBigBank/src/thebigbank/TheBigBank.java
@@ -14,6 +14,6 @@ public class TheBigBank {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("What is up my niggah? :S");
+        System.out.println("What is up my niggah? :/");
     }
 }
